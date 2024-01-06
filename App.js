18d39adf9545553e952d1baa14617705d2937e0f
Prashant Bhati <prashantbsmvit070@gmail.com>
2024-01-06 22:46:21 +0530

    commit after editing

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const Header=()=>{
     return (
         <div className="header">
             <div className="logo-container">
-                <img className="logo" src="https://img.freepik.com/premium-vector/hat-chef-logo-vector-illustration-design_598213-1794.jpg" alt="Image not found" />
+                <img className="logo" src="https://img.freepik.com/premium-photo/3d-style-burger-app-icon-game-design-logo-graphic-generative-ai_159242-29252.jpg" alt="Image not found" />
             </div>
             <div className="nav-items">
                 <ul>
@@ -37,13 +37,52 @@ const Header=()=>{
 
 }
 
+
+const RestaurantCard=()=>{
+    return (
+        <div className="res-card">
+            <img className="card-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bb7ae131544c7d37e10fc5faf76f09d6" alt="Image not found" />
+            <h2>Megana Foods</h2>
+            <h4>Biryani</h4>
+            <h4>4.5 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+const Body=()=>{
+    return (
+        <div className="body">
+            <div className="Search">
+                <p>Search bar</p>
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+
+        </div>
+    )
+}
+
 const AppLayout=()=>{
     return (
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(<AppLayout/>)
+
+
+
+//Config driven UI
+//THE UI we are viewing is based on the data we are receiving from backend.
